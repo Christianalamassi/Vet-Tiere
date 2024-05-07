e73406ba1f4c26d1f49565427b043f07547f1c96
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-07 15:22:40 +0000

    add static

--- blog/models.py
+++ blog/models.py
@@ -5,30 +5,9 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     name = models.CharField(max_length=255)
-    password = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="blog_posts"
-    )
-    phone = models.IntegerField(null=True)
     birth = models.DateField(null=True)
 
     def __str__(self):
         return self.name
 
-
-class user(models.Model):
-    name = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="user"
-    )
-    appointment = models.DateTimeField(null=True)
-    event = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="appointment"
-    )
-    def __str__(self):
-        return f"you have apointment {self.name}"
 
