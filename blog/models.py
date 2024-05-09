421b1bf820fc2ef91532ae5f1266fb811020d6ab
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-09 15:17:30 +0000

    chang in settings

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-#class user_info(models.Model):
-#    name = models.CharField(max_length=255)
-#   type = models.CharField(max_length=255)
-#    birth = models.DateField(null=True)
-#    appointment = models.DateField(null=True)
-#   
-#    def __str__(self):
-#        return self.name
-
-
+class user_info(models.Model):
+   Full_name = models.CharField(max_length=255)
+   User_name = models.CharField(max_length=255)
+   E_mail = models.CharField(max_length=255)
+   appointment = models.DateField(null=True)
+   def __str__(self):
+     return self.name
