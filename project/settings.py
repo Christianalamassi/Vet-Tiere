e63cb3fedab1eab5fadac7e87dddb068e6742481
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-09 17:14:55 +0000

    test deplying in heroku

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['8000-christianalamass-pp4-bsgxxnbcr4w.ws-eu111.gitpod.io',
-'ppfour.herokuapp.com']
+'.herokuapp.com']
 
 
 # Application definition
