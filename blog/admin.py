b21cb5902bb68cf592c32c7da587b3449f396913
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-13 10:43:03 +0000

    fix error in post is running

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from .models import userInfo
 
 
-@admin.register(userInfo)
-
-class userInfoAdmin(admin.ModelAdmin):
-    list_display = ('appointment','time')
+admin.site.register(userInfo)
--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class userInfo(models.Model):
-  name_of_the_pet = models.CharField(max_length=50)
-  appointment = models.DateField(blank=True, null=True)
-  time = models.DateField(blank=True, null=True)
+  kind_of_the_pet = models.CharField(max_length=50, null=False, blank=False)
+  appointment = models.DateField(blank=False, null=False)
+  time = models.DateField(blank=False, null=False)
   text = models.TextField(blank=True)
 
   def __str__(self):
-    return self.appointment.count()
-
-  def __str__(self):
-    return self.time.count()
+    return self.kind_of_the_pet
