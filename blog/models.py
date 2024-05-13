a73dceea1cc17c97612fda003eaef57c81765ff9
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-13 08:26:25 +0000

    message appears to the user after booking

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class userInfo(models.Model):
+  name_of_the_pet = models.CharField(max_length=50)
   appointment = models.DateField(blank=True, null=True)
   time = models.DateField(blank=True, null=True)
   text = models.TextField(blank=True)
--- blog/views.py
+++ blog/views.py
@@ -12,50 +12,18 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html')
 
-#sign in page
-def singup(request):
-    if request.method == 'POST':
-        nams = request.POST['name']
-        user_names = request.POST['user_name']
-        E_mails = request.POST['E_maill']
-        #phones = request.POST['phone']
-
-        return render(request, "blog/message.html", {
-            'names' : names,
-            'user_names' : user_names,
-            'E_mails'  :E_mails,
-            #'phones' : phones,
-            })
-        return render(request, 'blog/message.html')
-    else:
-        return render(request, 'blog/singup.html')
-        
-   
-#message appears when whaen the user sign up
-def message(request):
-    return render(request, 'blog/message.html')
-
-# log-in page
-def login(request):
-    if request.method == 'POST':
-        usernames = request.POST['username']
-        passwords= request.POST['password']
-        return render(request, "blog/login.html")
-    else:
-        return render(request, "blog/login.html")
 
 #Users page
 def user(request):
     models = userInfo
     if request.method == 'POST':
-        appointments = request.POST['appointment']
-        times = request.POST['times']
+        dates = request.POST['date']
+        times = request.POST['time']
         return render(request, "blog/message.html",{
-            'appointments' : appointments,
+            'dates' : dates,
             'times' : times})
     else:
         return render(request, "blog/user.html")
-            
-def appointment(request):
-    return render(request, 'blog/appointment.html')     
 
+def message(request):
+    return render(request, 'blog/message.html')     
