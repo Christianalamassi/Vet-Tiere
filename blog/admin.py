8a71fc8a9e09347ddec672a416a2eece3cbf2dc8
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-16 15:36:33 +0000

    add if user.is_authenticated to base and fix some spelling

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import userInfo
 
+@admin.register(userInfo)
+class user_appointment(admin.ModelAdmin):
+    list_display = (
+        'user', 'pet_name', 'date', 'time',
+        )
+    search_fields = ['user', 'pet_name']
+    list_filter = ('date',)
+    
 
-admin.site.register(userInfo)
