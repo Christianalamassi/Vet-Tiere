4b54c6fd420b4b643ab288f5d70c95a0bd784ffd
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-17 12:04:41 +0000

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import userInfo
-from django.views import generic
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
