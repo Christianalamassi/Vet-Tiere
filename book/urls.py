0b500911ef4052beb996c8aeb63e4f03e77e925a
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-05-28 10:47:55 +0000

    check Cl python

--- book/urls.py
+++ book/urls.py
@@ -1,15 +1,38 @@
 from django.urls import path
-from book import views
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from book import views
 
 
-urlpatterns = [ 
-    path ('',TemplateView.as_view(template_name='book/index.html'),name='homepage'),
-    path('about',views.about, name='about'),
-    path('appointment',views.appointment, name='appointment'),
-    path('message', views.message, name="message"),
-    path('edit_appointment/<int:pk>/',views.edit_appointment, name='edit_appointment'),
-    path('delete_appointment/<int:pk>/', views.delete_appointment, name='delete_appointment'),
-    path('delete_message', views.delete_message, name='delete_message')
+urlpatterns = [
+    path(
+        '', TemplateView.as_view(template_name='book/index.html'),
+        name='homepage'
+        ),
+
+    path(
+        'about', views.about, name='about'
+        ),
+
+    path(
+        'appointment', views.appointment, name='appointment'
+        ),
+
+    path(
+        'message', views.message, name="message"
+        ),
+
+    path(
+        'edit_appointment/<int:pk>/', views.edit_appointment,
+        name='edit_appointment'
+        ),
+
+    path(
+        'delete_appointment/<int:pk>/', views.delete_appointment,
+        name='delete_appointment'
+        ),
+
+    path(
+        'delete_message', views.delete_message, name='delete_message'
+    )
 ]
